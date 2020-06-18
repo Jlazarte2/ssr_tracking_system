83d68a1058fdb1e3c6e64d7f5101107871f98937
bernalnel <bernaljonnel23@gmail.com>
2020-06-19 07:36:31 +0800

    Updated PHP files.

--- PHP Files/connections.php
+++ PHP Files/connections.php
@@ -1,14 +1,12 @@
 <?php
     $servername = "localhost";
-    $username = "admin";
-    $password = "admin";
+    $username = "root";
+    $password = "";
     $databasename = "ssr_database";
 
     $connections = mysqli_connect($servername, $username, $password, $databasename);
     if ($connections-> connect_error) {
         die("Connection failed:". $conn-> connect_error);
         echo "Failed to connect to MySQL.";
-    } else {
-        echo "Connected.";
-    }
+    } 
 ?>
--- PHP Files/index.php
+++ PHP Files/index.php
@@ -1,63 +1,57 @@
 <?php
-  include ("connections.php");
-  $employee_id = $password = "";
+    $employee_id = "";
+    $password = "";
+    $employee_id_error = "";
+    $password_error = "";
 
-  if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    if(empty($_POST[employee_id])){
-      
-    }else{
-      $employee_id = $_POST["employee_id"];
-    }
-
-    if(empty($_POST[password])){
-
-    }else{
-      $password = $_POST["password"];
-    }
-  }
-
-
-  if($employee_id && $password){
-    include("connections.php");
-
-    $check_mail = mysqli_query($connections, "SELECT * FROM ssr_accounts WHERE employee_id='$employee_id'");
-    $check_mail_row = mysqli_num_rows($check_mail);
-
-    if($check_mail_row > 0) {
-      while($row = mysqli_fetch_assoc($check_mail)){
-        $db_password = $row["password"];
-
-        if($password == $db_password){
-
-          echo "Welcome admin.";
-        } else{
-          echo "Wrong passwprd."
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(empty($_POST["employee_id"])){
+            $employee_id_error = "Email is required!";
+        }else{
+            $employee_id = $_POST["employee_id"];
         }
-      }
-    }
-    else{
-      echo "Not registered.";
-    }
-  }
 
+        if(empty($_POST["password"])){
+            $password_error = "Password is required!";
+        }else{
+            $password = $_POST["password"];
+        }
+
+        if($employee_id && $password){
+            include ("connections.php");
+
+            $check_employee = mysqli_query($connections, "SELECT * FROM ssr_accounts WHERE employee_id='$employee_id'");
+            $check_employee_row = mysqli_num_rows($check_employee);
+
+            if($check_employee_row > 0){
+                while($row = mysqli_fetch_assoc($check_employee)){
+                    $db_password = $row["password"];
+                    $db_type = $row["type"];
+                    
+                    echo $password;
+                    echo $db_password;
+
+                        if($password != $db_password){
+                            $password_error = "Password is incorrect!";
+                            
+                        }else{
+                            $password_error = "Password is correct!";
+                        }
+                }
+            }else{
+                $employee_id_error = "Employee ID is not registered!";
+            }
+        }
+    }
 ?>
 
-<hr>
+<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-<html>
-  <head>
-      Login
-  </head>
+    <input type="text" name="employee_id" value="<?php echo $employee_id; ?>"> <br>
+    <span class="error"><?php echo $employee_id_error; ?></span> <br>
 
-  <form method="POST" action="<?php htmlspecialchars("PHP_SELF"); ?>">
-      <label for="username">Username:</label><br>
-      <input type="text" name="employee_id" value="<?php echo $employee_id; ?>" required><br>
-     
-      <label for="password">Password:</label><br>
-      <input type="password" name="password" value="<?php echo $password; ?>" require><br>
+    <input type="text" name="password" value="<?php echo $password; ?>"> <br>
+    <span class="error"><?php echo $password_error; ?></span> <br>
 
-      <button type="submit" value="Login">
-  </form>
-
-</html>
+    <input type="submit" value="Login">
+</form>

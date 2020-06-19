421b68ffddb12c35a5d69666bb88e9da205d3047
jlazarte2 <jlazarte2@dxc.com>
2020-06-19 14:30:53 +0800

    updated

--- php/index.php
+++ php/index.php
@@ -17,8 +17,6 @@
             $log_password = $_POST["password"];
         }
 
-        
-
 
         if($employee_id && $log_password){
             include ("connections.php");
@@ -31,12 +29,6 @@
                     $db_password = $row["password"];
                     $db_type = $row["type"];
                     
-
-                    echo $log_password;
-                    echo"<br>";
-                    echo $db_password;
-                    
-
                         if($log_password == $db_password){
                             $password_error = "Password is correct!";
                             
@@ -52,11 +44,10 @@
 ?>
 
 <form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-
-    <input type="text" name="employee_id" value="<?php echo $employee_id; ?>"> <br>
+    Email ID: <input type="text" name="employee_id" value="<?php echo $employee_id; ?>"> <br>
     <span class="error"><?php echo $employee_id_error; ?></span> <br>
 
-    <input type="text" name="password" value="<?php echo $log_password; ?>"> <br>
+    Password: <input type="password" name="password" value="<?php echo $log_password; ?>"> <br>
     <span class="error"><?php echo $password_error; ?></span> <br>
 
     <input type="submit" value="Login">

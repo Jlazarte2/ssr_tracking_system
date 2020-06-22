6710871b95c921460e659753a25b6fad58934d71
jlazarte2 <jlazarte2@dxc.com>
2020-06-22 10:18:19 +0800

    Update viewrecords.php

--- viewrecords.php
+++ viewrecords.php
@@ -1,8 +1,6 @@
 <?php
     include ("connections.php");
 
-    $view_query = mysqli_query($connections, "SELECT * FROM ssr_tracker");
-
     echo "<table border='5' width '0%'>";
     echo "<tr>
             <td>Date Received</td>
@@ -12,7 +10,31 @@
             <td>SRE Name</td>
         </tr>";
 
-    while($row = mysqli_fetch_assoc($view_query)){
+    if (isset($_GET["search"])){
+        $check = $_GET["search"];
+
+        $terms = explode(" ", $check);
+        $q = "SELECT * FROM ssr_tracker WHERE ";
+        $i = 0;
+
+       foreach($terms as $each){
+            $i++;
+            if($i==1){
+                $q .= "usyd_no LIKE '%$each%' ";
+            }
+            else{
+                $q .= "OR usyd_no LIKE '%$each%' ";
+            }
+        }
+
+        $query = mysqli_query($connections, $q);
+    }
+    else{
+        $check = "";
+        $query = mysqli_query($connections, "SELECT * FROM ssr_tracker");
+    }
+
+    while($row = mysqli_fetch_assoc($query)){
         $db_dxc_ssr = $row["dxc_ssr"];
         $db_date = $row["date"];
         $db_usyd_no = $row["usyd_no"];
@@ -20,12 +42,23 @@
         $db_sre_name = $row["sre_name"];
 
         echo "<tr>
-                <td>$db_date</td>
-                <td>$db_dxc_ssr</td>
-                <td>$db_usyd_no</td>
-                <td>$db_ssr_owner</td>
-                <td>$db_sre_name</td>
+            <td>$db_date</td>
+            <td>$db_dxc_ssr</td>
+            <td>$db_usyd_no</td>
+            <td>$db_ssr_owner</td>
+            <td>$db_sre_name</td>
             </tr>";
     }
     echo "</table>";
 ?>
+
+<form method="GET" action="viewrecords.php">
+
+    <input type="text" name="search" value="<?php echo $check; ?>" required><br>
+    <input type="submit" value="search">
+    
+     
+</form>
+<form method="post" action="viewrecords.php">
+<input type="submit" name="clear" value="Clear">
+</form>

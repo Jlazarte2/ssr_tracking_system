ae8ddb4a7be40613fa5cdaf8483687ba68f46842
Bermejo <abermejo2@dxc.com>
2020-07-02 11:18:30 +0800

    updated newrequest.php, searchrequest.php & Query to Db.txt

--- php/newrequest.php
+++ php/newrequest.php
@@ -42,12 +42,61 @@
         if($_POST["description"]){
             $perform = $_POST["description"];
         }
-            if($query = mysqli_query($connections, "INSERT INTO ssr_tracker(description, usyd_no, priority, applicable, sre_name, prior, action_after, ssr_owner, exec_date, start_time, end_time, usyd_cat, dxc_cat, perform) 
-            VALUES ('$description','$usyd_no','$priority','$applicable','$sre_name','$prior','$action_after','$ssr_owner','$exec_date','$start_time','$end_time','$usyd_cat','$dxc_cat','$perform')")){
 
+        //date & time in a different time zone e.g. Australia/sydney
+        //date_default_timezone_set('Australia/Sydney');
+        date_default_timezone_set('Asia/Manila');
+        $date = date('Y-m-d H:i:s');
+
+        //status of request
+        $status = 'For change creation';
         
-            
-    
+        //dxc_contact - PDLs
+        if($usyd_cat === "asa"){
+            $dxc_contact = "cyber_nss_unisyd@dxc.com";
+        }
+        if($usyd_cat === "backup"){
+            $dxc_contact = "itogdcgocphbursusyd@dxc.com";
+        }
+        if($usyd_cat === "oracle"){
+            $dxc_contact = "redrocksupport@dxc.com";
+        }
+        if($usyd_cat === "sql"){
+            $dxc_contact = "rocksolid.sqlsupport@dxc.com";
+        }
+        if($usyd_cat === "f5"){
+            $dxc_contact = "sim@dxc.com";
+        }
+        if($usyd_cat === "msv"){
+            $dxc_contact = "anz-ph-cloudops-uos@dxc.com";
+        }
+        if($usyd_cat === "nsx"){
+            $dxc_contact = "anz-ph-wintel-iaction@dxc.com";
+        }
+        if($usyd_cat === "aws"){
+            $dxc_contact = "dxc_in_aws_cloudops_pod1@dxc.com";
+        }
+        if($usyd_cat === "azure"){
+            $dxc_contact = "dxc_in_azure_cloudops_pod2@dxc.com";
+        }
+        if($usyd_cat === "storage"){
+            $dxc_contact = "itogdcgocphbursusyd@dxc.com";
+        }
+        if($usyd_cat === "unix"){
+            $dxc_contact = "BSS_ITO_GOC_PH_UNIX_USYD@dxc.com";
+        }
+        if($usyd_cat === "vmware"){
+            $dxc_contact = "anz-ph-wintel-iaction@dxc.com";
+        }
+        if($usyd_cat === "wintel"){
+            $dxc_contact = "anz-ph-wintel-iaction@dxc.com";
+        }
+        
+        
+            if($query = mysqli_query($connections, "INSERT INTO ssr_tracker(description, usyd_no, priority, applicable, sre_name, prior, action_after, ssr_owner, exec_date, start_time, end_time, usyd_cat, dxc_cat, perform, date, status, dxc_contact) 
+            VALUES ('$description','$usyd_no','$priority','$applicable','$sre_name','$prior','$action_after','$ssr_owner','$exec_date','$start_time','$end_time','$usyd_cat','$dxc_cat','$perform', '$date', '$status', '$dxc_contact')")){
+
+
 
                         //IMPORTANT CHANGE THE FOLDER OF USYD_NO TO DXCSSR
             // Uploads files

af3d0ee4d86a585b6c2e1bb2196f64d05c84d27d
jlazarte2 <jlazarte2@dxc.com>
2020-07-07 12:53:22 +0800

    l

--- php/get.php
+++ php/get.php
@@ -10,30 +10,17 @@
 include ("./connections.php");
 
 
-    
-    
+
+
     $query = mysqli_query($connections, "SELECT * FROM ssr_snow;");
     //Select all request
     echo "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>";
     while($row = mysqli_fetch_assoc($query)){
-        $sys_id = $row["sys_id"];
-        echo "<script type='text/javascript'normalget($sys_id)</script>";
-
-                 
-            }
-            echo "<script>
-                alert('New Ticket has been updated!');
-                window.location.href='../index.html';
-                </script>";
-                
+            $sys_id = $row["sys_id"];
+        echo "<script type='text/javascript'>normalget('$sys_id')</script>";
+    }
+        echo "<script>alert('Tickets are now updated')</script>";
+    header( "refresh:2; url=../searchrequest.php");
+    
 ?>
-
-
-<body onload="normalpost('<?php echo $category ?>',
-    '<?php echo $priority ?>', '<?php echo $category ?>', '<?php echo $sdescription ?>',
-    '<?php echo $time ?>', '<?php echo $time ?>', '<?php echo $dxc_ssr ?>')">
-
-</body>
-
-
 </html>
